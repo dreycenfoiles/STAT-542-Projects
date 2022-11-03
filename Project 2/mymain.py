8a0295ced1767ad48f3078d793088206b95cb8ff
Dreycen <foilesdreycen@gmail.com>
2022-11-02 23:35:23 -0500

    Fixed generatation of data

---
+++ Project 2/mymain.py
@@ -0,0 +1,83 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
+
+
+def mypredict(train, test, next_fold, t):
+
+    x_cols = ['Date', 'Store', 'Dept', 'IsHoliday']
+
+    start_date = pd.to_datetime("2011-03") + pd.DateOffset(months=2*t)
+    end_date = pd.to_datetime("2011-04") + pd.DateOffset(months=2*t)
+
+    date_filter = (test['Date'] > start_date) & (test['Date'] < end_date)
+
+    current_test = test[x_cols].copy().loc[date_filter]
+
+    tmp = pd.DataFrame()
+
+    if not isinstance(next_fold, type(None)):
+        next_fold = next_fold
+        train = pd.concat([train,next_fold])
+    
+    dates = train['Date']
+    tmp = train.copy()
+    tmp['Date'] = (dates - dates.min()).dt.days
+
+    current_test_dates = current_test['Date']
+    current_test['Date'] = (current_test_dates - dates.min()).dt.days 
+
+    xtrain = tmp[x_cols].values
+    ytrain = tmp['Weekly_Sales'].values 
+
+    model = LinearRegression() 
+
+    model.fit(xtrain, ytrain)
+
+    ypred = model.predict(current_test.values)
+    test.loc[date_filter, "Weekly_Pred"] = ypred 
+
+    return train, test 
+
+
+train = pd.read_csv('train_ini.csv', parse_dates=['Date'], index_col=[0])
+test = pd.read_csv('test.csv', parse_dates=['Date'], index_col=[0])
+
+
+# save weighed mean absolute error WMAE
+n_folds = 10
+next_fold = None
+wae = []
+
+# time-series prediction
+for t in range(1, n_folds+1):
+    print(f'Fold{t}...')
+
+    # *** THIS IS YOUR PREDICTION FUNCTION ***
+    train, test_pred = mypredict(train, test, next_fold, t-1)
+
+    # Load fold file
+    # You should add this to your training data in the next call to mypredict()
+    fold_file = 'fold_{t}.csv'.format(t=t)
+    next_fold = pd.read_csv(fold_file, parse_dates=['Date'], index_col=[0])
+
+    # extract predictions matching up to the current fold
+    scoring_df = next_fold.merge(
+        test_pred, on=['Date', 'Store', 'Dept'], how='left', suffixes=("", "_dummy"))
+
+    scoring_df.drop_duplicates()
+
+    print(scoring_df)
+
+    # extract weights and convert to numpy arrays for wae calculation
+    weights = scoring_df['IsHoliday'].apply(
+        lambda is_holiday: 5 if is_holiday else 1).to_numpy()
+    actuals = scoring_df['Weekly_Sales'].to_numpy()
+    preds = scoring_df['Weekly_Pred'].fillna(0).to_numpy()
+
+    wae.append(
+        (np.sum(weights * np.abs(actuals - preds)) / np.sum(weights)).item())
+
+print(wae)
+print(sum(wae)/len(wae))
