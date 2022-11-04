d71c1e13dd92f291ffe7ffdc9762cc4e9c16e60b
Dreycen <foilesdreycen@gmail.com>
2022-11-04 15:34:23 -0500

    Simple prediction model

--- Project 2/mymain.py
+++ Project 2/mymain.py
@@ -4,45 +4,69 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
 
 
-
 def mypredict(train, test, next_fold, t):
 
     if not isinstance(next_fold, type(None)):
         next_fold = next_fold
-        train = pd.concat([train,next_fold])
+        train = pd.concat([train, next_fold])
 
-    tmp_train = pd.DataFrame()
-    tmp_test = pd.DataFrame() 
-
-    tmp_train['Week'] = pd.to_datetime(train['Date']).dt.isocalendar().week
-    tmp_train[['Store','Dept','IsHoliday']] = train[['Store', 'Dept', 'IsHoliday']]
-
-    tmp_train = pd.get_dummies(tmp_train, columns=['Week','Store','Dept','IsHoliday'])
+    def get_week(date): 
+        
+        if date.isocalendar().year == 2010:
+            return date.isocalendar().week - 1
+        else: 
+            return date.isocalendar().week
 
     start_date = pd.to_datetime("2011-03") + pd.DateOffset(months=2*(t-1))
     end_date = pd.to_datetime("2011-05") + pd.DateOffset(months=2*(t-1))
 
-    date_filter = (test['Date'] >= start_date) & (test['Date'] < end_date)
+    date_filter1 = (test['Date'] >= start_date) & (test['Date'] < end_date)
 
-    test_pred = test.copy().loc[date_filter]
+    test_current = test.copy().loc[date_filter1].drop(columns=['IsHoliday'])
+    start_last_year = np.min(test_current['Date']) - pd.Timedelta(days=375)
+    end_last_year = np.max(test_current['Date']) - pd.Timedelta(days=350)
 
-    tmp_test = test.copy().loc[date_filter].drop(['Date'],axis=1)
-    tmp_test['Week'] = pd.to_datetime(test['Date']).dt.isocalendar().week
-    tmp_test = pd.get_dummies(tmp_test, columns=['Week','Store','Dept','IsHoliday'])
-    
-    tmp_test = tmp_test.reindex(columns=tmp_train.columns, fill_value=0)
+    date_filter2 = (train['Date'] >= start_last_year) & (
+        train['Date'] < end_last_year)
 
-    xtrain = tmp_train.values
-    ytrain = train['Weekly_Sales'].values
+    tmp_train = train.copy().loc[date_filter2]
 
-    model = LinearRegression()
+    tmp_train['Week'] = pd.to_datetime(tmp_train['Date']).apply(get_week)
+    tmp_train = tmp_train.rename(columns={'Weekly_Sales':'Weekly_Pred'})
+    tmp_train = tmp_train.drop(columns=['Date','IsHoliday'])
 
-    model.fit(xtrain, ytrain)
+    test_current['Week'] = pd.to_datetime(test_current['Date']).dt.isocalendar().week
 
-    ypred = model.predict(tmp_test.values)
-    test_pred.loc[date_filter, "Weekly_Pred"] = ypred
+    test_pred = test_current.merge(
+        tmp_train, on=['Week', 'Store', 'Dept'], how='left')
 
     return train, test_pred
+
+
+# mypredict = function(){
+
+#     start_date < - ymd("2011-03-01") % m+% months(2 * (t - 1))
+#     end_date < - ymd("2011-05-01") % m+% months(2 * (t - 1))
+#     test_current < - test % > %
+#     filter(Date >= start_date & Date < end_date) % > %
+#     select(-IsHoliday)
+
+#     start_last_year = min(test_current$Date) - 375
+#     end_last_year = max(test_current$Date) - 350
+#     tmp_train < - train % > %
+#     filter(Date > start_last_year & Date < end_last_year) % > %
+#     mutate(Wk=ifelse(year(Date) == 2010, week(Date)-1, week(Date))) % > %
+#     rename(Weekly_Pred=Weekly_Sales) % > %
+#     select(-Date, -IsHoliday)
+
+#     test_current < - test_current % > %
+#     mutate(Wk=week(Date))
+
+#     test_pred < - test_current % > %
+#     left_join(tmp_train, by=c('Dept', 'Store', 'Wk')) % > %
+#     select(-Wk)
+#     return(test_pred)
+# }
 
 
 train = pd.read_csv('train_ini.csv', parse_dates=['Date'], index_col=[0])
@@ -58,7 +82,7 @@ for t in range(1, n_folds+1):
     print(f'Fold{t}...')
 
     # *** THIS IS YOUR PREDICTION FUNCTION ***
-    train, test_pred = mypredict(train, test, next_fold, t-1)
+    train, test_pred = mypredict(train, test, next_fold, t)
 
     # Load fold file
     # You should add this to your training data in the next call to mypredict()
@@ -67,7 +91,7 @@ for t in range(1, n_folds+1):
 
     # extract predictions matching up to the current fold
     scoring_df = next_fold.merge(
-        test_pred, on=['Date', 'Store', 'Dept'], how='left', suffixes=("", "_dummy"))
+        test_pred, on=['Date', 'Store', 'Dept'], how='left')
 
     # extract weights and convert to numpy arrays for wae calculation
     weights = scoring_df['IsHoliday'].apply(
